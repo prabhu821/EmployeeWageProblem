3041eedc40b5a387dd54ad9c02f8098f1ce50655
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 17:14:01 +0530

    [Prabhu] Add: Attendance

---
+++ ProblemEmployeeWage/EmployeeWage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemEmployeeWage;
+
+public class EmployeeWage
+{
+    const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, HALF_DAY_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, MONTH_WORK_DAY = 20;
+    Random random = new Random();
+
+    //UC1
+    public void Attendence()
+    {
+        int empCheck = random.Next(0, 2);
+        if (empCheck == 0)
+            Console.WriteLine("Employee is Present");
+        else
+            Console.WriteLine("Employee is Absent");
+    }
+}
--- ProblemEmployeeWage/Program.cs
+++ ProblemEmployeeWage/Program.cs
@@ -4,6 +4,23 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Employee Wage Computation Proble");
+        EmployeeWage employeeWage = new EmployeeWage();
+        Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nSelect the option");
+            Console.WriteLine("1.Attendance \n2.Exit");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+                case 1:
+                    employeeWage.Attendence();
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
